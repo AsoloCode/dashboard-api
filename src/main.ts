a8d99c2674f5f75408f711a2e6fee00e8ef9daa5
AsoloCode <artem-artem14@yandex.ru>
2023-07-07 22:12:45 +0300

    decorator
    inject

--- src/main.ts
+++ src/main.ts
@@ -8,19 +8,14 @@ import {ILogger} from "./logger/logger.interface";
 import {IExeptionFilter} from "./error/exeption.filter.interface";
 
 
-async function bootstrap() {
-    // const logger = new LoggerService()
-    // const app = new App(
-    //     logger,
-    //     new UsersController(logger),
-    //     new ExeptionFilter(logger))
+
     const appContainer = new Container();
     appContainer.bind<ILogger> (TYPES.ILogger).to (LoggerService);
     appContainer.bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
     appContainer.bind<UsersController>(TYPES.UserController).to(UsersController);
     appContainer.bind<App> (TYPES.Application) .to (App);
     const app = appContainer.get<App> (TYPES.Application);
-    await app.init()
-}
+    app.init()
 
-bootstrap()
+export {app, appContainer}
+
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,15 @@
 import {BaseController} from "../common/base.controller";
 import {LoggerService} from "../logger/logger.service";
 import {NextFunction, Request, Response} from "express";
+import {inject, injectable} from "inversify";
+import {TYPES} from "../../types";
+import {ILogger} from "../logger/logger.interface";
+import 'reflect-metadata'
 
+@injectable()
 export class UsersController extends BaseController {
-    constructor(logger: LoggerService) {
-        super(logger)
+    constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+        super(loggerService)
         this.bindRoutes([
                 {path: '/login', method: 'post', func: this.login},
                 {path: '/register', method: 'post', func: this.register}
