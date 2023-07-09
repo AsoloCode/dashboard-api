e10cfc0d5c200eee1f56984ea704d5edfc7d8190
AsoloCode <artem-artem14@yandex.ru>
2023-07-09 12:45:54 +0300

    eslint

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,31 @@
-import {App} from "./app";
-import {LoggerService} from "./logger/logger.service";
-import {UserController} from "./users/users.controller";
-import {ExeptionFilter} from "./error/exeption.filter";
-import {TYPES} from "../types";
-import {Container, ContainerModule, interfaces} from "inversify";
-import {ILogger} from "./logger/logger.interface";
-import {IExeptionFilter} from "./error/exeption.filter.interface";
-import {IUserController} from "./users/userController.interface";
+import { App } from './app';
+import { LoggerService } from './logger/logger.service';
+import { UserController } from './users/users.controller';
+import { ExeptionFilter } from './error/exeption.filter';
+import { TYPES } from '../types';
+import { Container, ContainerModule, interfaces } from 'inversify';
+import { ILogger } from './logger/logger.interface';
+import { IExeptionFilter } from './error/exeption.filter.interface';
+import { IUserController } from './users/userController.interface';
 
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
-    bind<ILogger> (TYPES.ILogger).to (LoggerService);
-    bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
-    bind<IUserController>(TYPES.UserController).to(UserController);
-    bind<App> (TYPES.Application).to (App);
-})
+	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+	bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<App>(TYPES.Application).to(App);
+});
 
 export interface IBootstrapReturn {
-    appContainer: Container;
-    app: App;
+	appContainer: Container;
+	app: App;
 }
 
 function bootstrap(): IBootstrapReturn {
-    const appContainer = new Container();
-    appContainer.load(appBindings);
-    const app = appContainer.get<App>(TYPES.Application);
-    app.init();
-    return { appContainer, app };
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	app.init();
+	return { appContainer, app };
 }
 
 export const { app, appContainer } = bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,37 +1,26 @@
-import {BaseController} from "../common/base.controller";
-import {LoggerService} from "../logger/logger.service";
-import {NextFunction, Request, Response} from "express";
-import {inject, injectable} from "inversify";
-import {TYPES} from "../../types";
-import {ILogger} from "../logger/logger.interface";
-import 'reflect-metadata'
-import {IUserController} from "./userController.interface";
+import { BaseController } from '../common/base.controller';
+import { NextFunction, Request, Response } from 'express';
+import { inject, injectable } from 'inversify';
+import { TYPES } from '../../types';
+import 'reflect-metadata';
+import { ILogger } from '../logger/logger.interface';
+import { IUserController } from './userController.interface';
 
 @injectable()
-export class UserController extends BaseController implements IUserController{
-    constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
-        super(loggerService)
-        this.bindRoutes([
-                {path: '/login', method: 'post', func: this.login},
-                {path: '/register', method: 'post', func: this.register}
-            ]
-        )
+export class UserController extends BaseController implements IUserController {
+	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+		super(loggerService);
+		this.bindRoutes([
+			{ path: '/login', method: 'post', func: this.login },
+			{ path: '/register', method: 'post', func: this.register },
+		]);
+	}
 
+	login(req: Request, res: Response, next: NextFunction): void {
+		this.ok(res, 'login');
+	}
 
-    }
-
-    login(req: Request, res: Response, next: NextFunction) {
-        this.ok(res, 'login')
-    }
-
-    register(req: Request, res: Response, next: NextFunction) {
-        this.ok(res, 'register')
-    }
-
+	register(req: Request, res: Response, next: NextFunction): void {
+		this.ok(res, 'register');
+	}
 }
-
-
-
-
-
-
