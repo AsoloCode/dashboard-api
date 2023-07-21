5e6f93a31e89b9d5814ed9a95694f3828140f276
AsoloCode <artem-artem14@yandex.ru>
2023-07-21 14:55:28 +0300

    refac

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,13 +1,12 @@
 import { IConfigService } from './config.service.interface';
 import { config, DotenvConfigOutput, DotenvParseOutput } from 'dotenv';
 import { inject, injectable } from 'inversify';
-import { TYPES } from '../../types';
+import { TYPES } from '../types';
 import { ILogger } from '../logger/logger.interface';
 
 @injectable()
 export class ConfigService implements IConfigService {
 	private config: DotenvParseOutput;
-
 	constructor(@inject(TYPES.ILogger) private logger: ILogger) {
 		const result: DotenvConfigOutput = config();
 		if (result.error) {
